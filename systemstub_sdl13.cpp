d892cc9f19e2173745781894e28186c73ecbe426
Gregory Montoir <cyx@users.sourceforge.net>
2010-02-06 18:24:28 +0100

    add some TODOs to SDL13

--- systemstub_sdl13.cpp
+++ systemstub_sdl13.cpp
@@ -1,20 +1,6 @@
-/* REminiscence - Flashback interpreter
- * Copyright (C) 2005-2010 Gregory Montoir
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
+// TODO: move common code to systemstub_sdlcommon.cpp (maybe just regular C functions)
+// TODO: rename systemstub_sdl.cpp to systemstub_sdl12.cpp
 
 #include <SDL.h>
 #include "scaler.h"
