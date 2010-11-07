ce6d3c4a6633ff60599a921b1cd6f126b4af8f74
Gregory Montoir <cyx@users.sourceforge.net>
2010-11-07 16:18:06 +0100

    cleanup mac decoders

--- tools/convert_mac_png/decode.cpp
+++ tools/convert_mac_png/decode.cpp
@@ -5,7 +5,7 @@
 #include "file.h"
 
 uint8_t *decodeLzss(File &f) {
-	uint32_t decodedSize = f.readUint32BE();
+	const uint32_t decodedSize = f.readUint32BE();
 	printf("decodeLzss decodedSize %d\n", decodedSize);
 	uint8_t *dst = (uint8_t *)malloc(decodedSize);
 	int count = 0;
@@ -36,16 +36,13 @@ void decodeC103(const uint8_t *a3, uint8_t *a0, int w, int h) {
 	int d7 = 1;
 	int d6 = 0;
 	int d1 = 0;
-	// d4 width
-	// d5 height
-	uint32_t d5 = 0xFFF;
+	static const uint32_t d5 = 0xFFF;
 	uint8_t a1[0x1000];
 
 	for (int y = 0; y < h; ++y) {
 		a0 = baseA0 + y * w;
-//printf("Y=%d\n", y);
 		for (int x = 0; x < w; ++x) {
-assert(d6 >= 0);
+			assert(d6 >= 0);
 			if (d6 == 0) {
 				int carry = d7 & 1;
 				d7 >>= 1;
@@ -68,13 +65,10 @@ assert(d6 >= 0);
 				d6 = d1;
 				d1 &= d5;
 				++d1;
-				d1 = -d1;
-				d1 += d3;
-				d1 &= d5;
+				d1 = (d3 - d1) & d5;
 				d6 >>= 12;
 				d6 += 3;
 			}
-//loc_384:
 			d0 = a1[d1++];
 			d1 &= d5;
 			a1[d3++] = d0;
@@ -85,58 +79,45 @@ assert(d6 >= 0);
 	}
 }
 
-
-static struct {
-	const uint8_t *ptr;
-	int counter;
-} stack[512];
-
-static int sp = 0;
-
-static void stack_push(const uint8_t *ptr, int counter) {
-	assert(sp < sizeof(stack) / sizeof(stack[0]));
-	stack[sp].ptr = ptr;
-	stack[sp].counter = counter;
-	++sp;
-}
-
 void decodeC211(const uint8_t *a3, uint8_t *a0, int pitch, int h) {
+	static struct {
+		const uint8_t *ptr;
+		int counter;
+	} stack[512];
 	uint8_t d0;
-	int d1;
-	int carry_set;
 	uint8_t *baseA0 = a0;
 	int Y = 0;
+	int SP = 0;
 
 	while (1) {
 		d0 = *a3++;
-//printf("d0=0x%02x a3=%p\n", d0, a3);
 		if ((d0 & 0x80) != 0) {
 			++Y;
-//printf("Y %d\n", Y);
 			a0 = baseA0 + Y * pitch;
 		}
-		d1 = d0 & 0x1F;
+		int d1 = d0 & 0x1F;
 		if (d1 == 0) {
 			d1 = a3[0] * 256 + a3[1];
 			a3 += 2;
 		}
-		carry_set = (d0 & 0x40) != 0;
+		const int carry_set = (d0 & 0x40) != 0;
 		d0 <<= 2;
-//printf("d1=0x%02x carry=%d d0&0x80=%d\n", d1, carry_set, (d0 & 0x80) != 0);
 		if (!carry_set) {
 			if ((d0 & 0x80) == 0) {
 				--d1;
 				if (d1 == 0) {
-//					printf("SP %d\n", sp);
-					assert(sp > 0);
-					--stack[sp - 1].counter;
-					if (stack[sp - 1].counter >= 0) {
-						a3 = stack[sp - 1].ptr;
+					assert(SP > 0);
+					--stack[SP - 1].counter;
+					if (stack[SP - 1].counter >= 0) {
+						a3 = stack[SP - 1].ptr;
 					} else {
-						--sp;
+						--SP;
 					}
 				} else {
-					stack_push(a3, d1); // sp -= 6;
+					assert(SP < sizeof(stack) / sizeof(stack[0]));
+					stack[SP].ptr = a3;
+					stack[SP].counter = d1;
+					++SP;
 				}
 			} else {
 				a0 += d1;
