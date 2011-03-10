5938fde496dfc1baf0b614e6239e26dc885e020d
Gregory Montoir <cyx@users.sourceforge.net>
2011-03-10 12:20:45 +0100

    add Amiga room background mask

--- video.cpp
+++ video.cpp
@@ -480,7 +480,7 @@ static const uint8 *AMIGA_mirrorX(const uint8 *a2) {
 	return buf;
 }
 
-static void AMIGA_blit4p8x8(uint8 *dst, int pitch, const uint8 *src, int colorKey = -1) {
+static void AMIGA_blit4p8x8(uint8 *dst, int pitch, const uint8 *src, bool masked, int colorKey = -1) {
 	for (int y = 0; y < 8; ++y) {
 		for (int i = 0; i < 8; ++i) {
 			const int mask = 1 << (7 - i);
@@ -491,7 +491,7 @@ static void AMIGA_blit4p8x8(uint8 *dst, int pitch, const uint8 *src, int colorKe
 				}
 			}
 			if (color != colorKey) {
-				dst[i] = color;
+				dst[i] = masked ? color + 0x80 : color;
 			}
 		}
 		++src;
@@ -514,7 +514,8 @@ static void AMIGA_decodeLevHelper(uint8 *dst, const uint8 *src, int offset10, in
 					if ((d3 & (1 << 11)) != 0) {
 						a2 = AMIGA_mirrorX(a2);
 					}
-					AMIGA_blit4p8x8(dst + y * 256 + x, 256, a2);
+					const bool masked = (d3 < (1 << 15)) == 0;
+					AMIGA_blit4p8x8(dst + y * 256 + x, 256, a2, masked);
 				}
 			}
 		}
@@ -536,7 +537,8 @@ static void AMIGA_decodeLevHelper(uint8 *dst, const uint8 *src, int offset10, in
 					if ((d3 & (1 << 11)) != 0) {
 						a2 = AMIGA_mirrorX(a2);
 					}
-					AMIGA_blit4p8x8(dst + y * 256 + x, 256, a2, 0);
+					const bool masked = (d3 < (1 << 15)) == 0;
+					AMIGA_blit4p8x8(dst + y * 256 + x, 256, a2, masked, 0);
 				}
 			}
 		}
@@ -605,6 +607,7 @@ void Video::AMIGA_decodeLev(int level, int room) {
 		setPaletteSlotBE(i, _mapPalSlot2);
 	}
 	setPaletteSlotBE(0x6, _mapPalSlot2);
+	setPaletteSlotBE(0x8, num[0]);
 	setPaletteSlotBE(0xA, _mapPalSlot2);
 }
 
