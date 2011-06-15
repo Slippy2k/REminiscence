3e3990ef049dabbdc92e5be140e79342d8398ffd
Gregory Montoir <cyx@users.sourceforge.net>
2011-06-15 21:08:38 +0200

    decode_dpoly: print vertices as signed numbers

--- tools/decode_dpoly/decode_dpoly.c
+++ tools/decode_dpoly/decode_dpoly.c
@@ -1,29 +1,24 @@
 
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
-
-typedef signed char int8;
-typedef unsigned char uint8;
-typedef signed short int16;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
 
 static FILE *fp_CMD;
 static FILE *fp_POL;
 
-static uint8 freadByte(FILE *fp) {
+static uint8_t freadByte(FILE *fp) {
 	return fgetc(fp);
 }
 
-static uint16 freadUint16BE(FILE *fp) {
-	uint16 n = fgetc(fp) << 8;
+static uint16_t freadUint16BE(FILE *fp) {
+	uint16_t n = fgetc(fp) << 8;
 	n |= fgetc(fp);
 	return n;
 }
 
-static void printPalette(uint16 num) {
+static void printPalette(uint16_t num) {
 	int i;
-	uint16 offset, color;
+	uint16_t offset, color;
 
 	fseek(fp_POL, 0x6, SEEK_SET);
 	offset = freadUint16BE(fp_POL);
@@ -34,40 +29,40 @@ static void printPalette(uint16 num) {
 	}
 }
 
-static void printPolygon() {
+static void printShapeVertices() {
 	int num;
-	int8 dx, dy;
-	int16 x, y, rx, ry;
+	int8_t dx, dy;
+	int16_t x, y, rx, ry;
 	
 	num = freadByte(fp_POL);
 	if (num & 0x80) {
-		x = freadUint16BE(fp_POL);
-		y = freadUint16BE(fp_POL);
+		x = (int16_t)freadUint16BE(fp_POL);
+		y = (int16_t)freadUint16BE(fp_POL);
 		rx = freadUint16BE(fp_POL);
 		ry = freadUint16BE(fp_POL);
-		printf("\n  ellipse ( x=%X,y=%X, rx=%X,ry=%X )", x, y, rx, ry);
+		printf("\n  ellipse ( x=%d,y=%d, rx=%d,ry=%d )", x, y, rx, ry);
 	} else if (num == 0) {
-		x = freadUint16BE(fp_POL);
-		y = freadUint16BE(fp_POL);
-		printf("\n  point ( x=%X,y=%X )", x, y);
+		x = (int16_t)freadUint16BE(fp_POL);
+		y = (int16_t)freadUint16BE(fp_POL);
+		printf("\n  point ( x=%d,y=%d )", x, y);
 	} else {
-		x = freadUint16BE(fp_POL);
-		y = freadUint16BE(fp_POL);
+		x = (int16_t)freadUint16BE(fp_POL);
+		y = (int16_t)freadUint16BE(fp_POL);
 		printf("\n  polygon ( ");
 		for (--num; num >= 0; --num) {
-			dx = freadByte(fp_POL);
-			dy = freadByte(fp_POL);
-			printf("x=%X,y=%X ", x + dx, y + dy);
+			dx = (int8_t)freadByte(fp_POL);
+			dy = (int8_t)freadByte(fp_POL);
+			printf("x=%d,y=%d ", x + dx, y + dy);
 		}
 		printf(" )");
 	}	
 }
 
-static void printVertices(uint16 shapeOffset) {
+static void printShape(uint16_t shapeOffset) {
 	int i, count;
-	int16 x, y, alpha, color;
-	uint32 previousOffset;
-	uint16 offset, verticesOffset;
+	int16_t x, y, alpha, color;
+	uint32_t previousOffset;
+	uint16_t offset, verticesOffset;
 		
 	fseek(fp_POL, 0x2, SEEK_SET);
 	offset = freadUint16BE(fp_POL);
@@ -82,8 +77,8 @@ static void printVertices(uint16 shapeOffset) {
 	for (i = 0; i < count; ++i) {
 		verticesOffset = freadUint16BE(fp_POL);
 		if (verticesOffset & 0x8000) {
-			x = freadUint16BE(fp_POL);
-			y = freadUint16BE(fp_POL);
+			x = (int16_t)freadUint16BE(fp_POL);
+			y = (int16_t)freadUint16BE(fp_POL);
 		} else {
 			x = 0;
 			y = 0;
@@ -102,16 +97,16 @@ static void printVertices(uint16 shapeOffset) {
 		offset = freadUint16BE(fp_POL);
 		fseek(fp_POL, offset + verticesOffset, SEEK_SET);
 
-		printf("\n  vertex %d/%d x %d y %d a %d c %d", i, count, x, y, alpha, color);
-		printPolygon();
+		printf("\n  shape %d/%d x %d y %d a %d c %d", i, count, x, y, alpha, color);
+		printShapeVertices();
 
 		fseek(fp_POL, previousOffset, SEEK_SET);
 	}
 }
 
 static void decodeDPoly(FILE *fp, const char *sequenceName, int num) {
-	uint8 op;
-	uint32 addr;
+	uint8_t op;
+	uint32_t addr;
 	int offset1, offset2;
 
 	if (num != 0) {
@@ -141,18 +136,18 @@ static void decodeDPoly(FILE *fp, const char *sequenceName, int num) {
 			}
 			break;
 		case 1: {
-				uint8 clr = freadByte(fp);
+				uint8_t clr = freadByte(fp);
 				printf("REFRESH_SCREEN ( clear_screen=%d )", clr);
 			}
 			break;
 		case 2: {
-				uint8 delay = freadByte(fp);
+				uint8_t delay = freadByte(fp);
 				printf("WAIT_FOR_SYNC ( delay=%d )", delay);
 			}
 			break;
 		case 3: {
-				int16 x, y;
-				uint16 shapeOffset = freadUint16BE(fp);
+				int16_t x, y;
+				uint16_t shapeOffset = freadUint16BE(fp);
 				if (shapeOffset & 0x8000) {
 					x = freadUint16BE(fp);
 					y = freadUint16BE(fp);
@@ -161,12 +156,12 @@ static void decodeDPoly(FILE *fp, const char *sequenceName, int num) {
 					y = 0;
 				}
 				printf("DRAW_SHAPE ( offset=0x%X, x=%d, y=%d )", shapeOffset & 0x7FFF, x, y);
-				printVertices(shapeOffset);
+				printShape(shapeOffset);
 			}
 			break;
 		case 4: {
-				uint8 spal = freadByte(fp);
-				uint8 dpal = freadByte(fp);
+				uint8_t spal = freadByte(fp);
+				uint8_t dpal = freadByte(fp);
 				printf("SET_PALETTE ( spal=%d, dpal=%d )", spal, dpal);
 				printPalette(spal);
 			}
@@ -176,7 +171,7 @@ static void decodeDPoly(FILE *fp, const char *sequenceName, int num) {
 			}
 			break;
 		case 6: {
-				uint16 str = freadUint16BE(fp);
+				uint16_t str = freadUint16BE(fp);
 				printf("DRAW_STRING_AT_BOTTOM ( str=%d )", str);
 			}
 			break;
@@ -195,8 +190,8 @@ static void decodeDPoly(FILE *fp, const char *sequenceName, int num) {
 			}
 			break;
 		case 10: {
-				int16 x, y, z, ix, iy;
-				uint16 shapeOffset = freadUint16BE(fp);
+				int16_t x, y, z, ix, iy;
+				uint16_t shapeOffset = freadUint16BE(fp);
 				if (shapeOffset & 0x8000) {
 					x = freadUint16BE(fp);
 					y = freadUint16BE(fp);
@@ -208,12 +203,12 @@ static void decodeDPoly(FILE *fp, const char *sequenceName, int num) {
 				ix = freadUint16BE(fp);
 				iy = freadUint16BE(fp);
 				printf("DRAW_SHAPE_SCALE ( offset=0x%X, x=%d, y=%d, z=%d, ix=%d, iy=%d )", shapeOffset & 0x7FFF, x, y, z, ix, iy);
-				printVertices(shapeOffset);
+				printShape(shapeOffset);
 			}
 			break;
 		case 11: {
-				int16 x, y, z, ix, iy, r1, r2, r3;
-				uint16 shapeOffset = freadUint16BE(fp);
+				int16_t x, y, z, ix, iy, r1, r2, r3;
+				uint16_t shapeOffset = freadUint16BE(fp);
 				if (shapeOffset & 0x8000) {
 					x = freadUint16BE(fp);
 					y = freadUint16BE(fp);
@@ -239,7 +234,7 @@ static void decodeDPoly(FILE *fp, const char *sequenceName, int num) {
 					r3 = 90;
 				}
 				printf("DRAW_SHAPE_SCALE_ROTATE ( offset=0x%X, x=%d, y=%d, z=%d, ix=%d, iy=%d, r1=%d, r2=%d, r3=%d )", shapeOffset & 0x7FFF, x, y, z, ix, iy, r1, r2, r3);
-				printVertices(shapeOffset);
+				printShape(shapeOffset);
 			}
 			break;
 		case 12: {
@@ -247,8 +242,8 @@ static void decodeDPoly(FILE *fp, const char *sequenceName, int num) {
 			}
 			break;
 		case 13: {
-				int16 x, y;
-				uint16 str = freadUint16BE(fp);
+				int16_t x, y;
+				uint16_t str = freadUint16BE(fp);
 				if (str != 0xFFFF) {
 					x =  freadUint16BE(fp) * 8;
 					y =  freadUint16BE(fp) * 8;
@@ -269,11 +264,11 @@ static void decodeDPoly(FILE *fp, const char *sequenceName, int num) {
 }
 
 int main(int argc, char *argv[]) {
-	if (argc == 2) {
+	if (argc == 4) {
 		fp_CMD = fopen(argv[1], "rb");
-		fp_POL = fopen("OBJET.POL", "rb");
+		fp_POL = fopen(argv[2], "rb");
 		if (fp_CMD && fp_POL) {
-			decodeDPoly(fp_CMD, argv[1], 4); /* XXX 4 for objet */
+			decodeDPoly(fp_CMD, argv[1], atoi(argv[3]));
 		}
 	}
 	return 0;
