a0cd4231711c4d1f576815241bcc0b799e4b4dfb
Gregory Montoir <cyx@users.sourceforge.net>
2015-02-25 21:50:04 +0800

    proto: remove unneeded DBG_ mask

--- proto/util.h
+++ proto/util.h
@@ -6,17 +6,10 @@
 
 enum {
 	DBG_INFO   = 1 << 0,
-	DBG_RES    = 1 << 1,
-	DBG_MENU   = 1 << 2,
-	DBG_UNPACK = 1 << 3,
-	DBG_PGE    = 1 << 4,
-	DBG_VIDEO  = 1 << 5,
-	DBG_GAME   = 1 << 6,
-	DBG_COL    = 1 << 7,
-	DBG_SND    = 1 << 8,
-	DBG_CUT    = 1 << 9,
-	DBG_MOD    = 1 << 10,
-	DBG_SFX    = 1 << 11
+	DBG_PGE    = 1 << 1,
+	DBG_GAME   = 1 << 2,
+	DBG_COL    = 1 << 3,
+	DBG_SND    = 1 << 4,
 };
 
 extern uint16_t g_debugMask;
