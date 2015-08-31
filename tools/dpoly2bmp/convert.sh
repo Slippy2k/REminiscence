6f59ff57d57bf43a037af6ae277d72e24dffd7a2
Gregory Montoir <cyx@users.sourceforge.net>
2015-08-31 18:20:32 +0800

    dpoly2bmp: convert all .SET files

--- tools/dpoly2bmp/convert.sh
+++ tools/dpoly2bmp/convert.sh
@@ -3,10 +3,16 @@
 do_file () {
 	fn=$1
 	./dpoly2bmp $fn
+	rm -f $fn.mp4
 	ffmpeg -framerate 10 -pattern_type glob -i '*.PNG' -c:v libx264 $fn.mp4
 	rm -f *.BMP *.PNG *.RGBA
 }
 
 do_file CAILLOU-F.SET
+do_file MEMOTECH.SET
+do_file MEMOSTORY0.SET
+do_file MEMOSTORY1.SET
+do_file MEMOSTORY2.SET
+do_file MEMOSTORY3.SET
 do_file JUPITERStation1.set
 do_file TAKEMecha1-F.set
