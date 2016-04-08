40ecff8a32f5ab8f5b710ed898b47c5becd5a21a
Gregory Montoir <cyx@users.sourceforge.net>
2016-04-08 08:44:53 +0800

    rs: add --language

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@ static const char *USAGE =
 	"  --levelnum=NUM    Start level (default '0')\n"
 	"  --fullscreen      Start fullscreen\n"
 	"  --scaler=INDEX    Graphics scaler\n"
+	"  --language=FR|EN|DE|SP|IT\n"
 ;
 
 static int detectVersion(FileSystem *fs) {
@@ -130,6 +131,7 @@ int main(int argc, char *argv[]) {
 	int levelNum = 0;
 	int scaler = DEFAULT_SCALER;
 	bool fullscreen = false;
+	int forcedLanguage = -1;
 	if (argc == 2) {
 		// data path as the only command line argument
 		struct stat st;
@@ -144,6 +146,7 @@ int main(int argc, char *argv[]) {
 			{ "levelnum",   required_argument, 0, 3 },
 			{ "fullscreen", no_argument,       0, 4 },
 			{ "scaler",     required_argument, 0, 5 },
+			{ "language",   required_argument, 0, 6 },
 			{ 0, 0, 0, 0 }
 		};
 		int index;
@@ -170,6 +173,26 @@ int main(int argc, char *argv[]) {
 				scaler = DEFAULT_SCALER;
 			}
 			break;
+		case 6: {
+				static const struct {
+					int lang;
+					const char *str;
+				} languages[] = {
+					{ LANG_FR, "FR" },
+					{ LANG_EN, "EN" },
+					{ LANG_DE, "DE" },
+					{ LANG_SP, "SP" },
+					{ LANG_IT, "IT" },
+					{ -1, 0 }
+				};
+				for (int i = 0; languages[i].str; ++i) {
+					if (strcasecmp(languages[i].str, optarg) == 0) {
+						forcedLanguage = languages[i].lang;
+						break;
+					}
+				}
+			}
+			break;
 		default:
 			printf(USAGE, argv[0]);
 			return 0;
@@ -183,7 +206,7 @@ int main(int argc, char *argv[]) {
 		error("Unable to find data files, check that all required files are present");
 		return -1;
 	}
-	Language language = detectLanguage(&fs);
+	const Language language = (forcedLanguage == -1) ? detectLanguage(&fs) : (Language)forcedLanguage;
 	SystemStub *stub = SystemStub_SDL_create();
 	Game *g = new Game(stub, &fs, savePath, levelNum, (ResourceType)version, language);
 	stub->init(g_caption, Video::GAMESCREEN_W, Video::GAMESCREEN_H, scaler, fullscreen);
