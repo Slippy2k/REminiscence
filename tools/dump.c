15ace42b0479be0ba25d11b826fa6483c2011a32
Gregory Montoir <cyx@users.sourceforge.net>
2016-04-13 09:03:50 +0800

    tools: dump creditsData from Amiga English binary

--- tools/dump.c
+++ tools/dump.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
+/* FlashBack Amiga_EN _creditsData */
+static const int kPos = 0x29D48;
+static const int kEnd = 0x2A6F0;
+
 int main(int argc, char *argv[]) {
 	if (argc >= 2) {
 		int count = 0;
 		FILE *fp = fopen(argv[1], "rb");
 		if (fp) {
-			fseek(fp, 0x1D508, SEEK_SET);
-			while (ftell(fp) <= 0x1E31B) {
+			fseek(fp, kPos, SEEK_SET);
+			while (ftell(fp) <= kEnd) {
 				if ((count % 16) == 0) {
 					printf("\n\t");
+				} else {
+					printf(" ");
 				}
-				printf("0x%02X, ", fgetc(fp));
+				printf("0x%02X,", fgetc(fp));
 				++count;
 			}
 			fclose(fp);
